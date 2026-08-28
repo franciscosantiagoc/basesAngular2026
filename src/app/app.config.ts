f84df484d8b9d96c97a7e418a90e44a7da016c40
Francisco Santiago <fcosantiagoc@gmail.com>
2026-08-28 03:51:55 -0600

    Feat (deploy): uso de fichero redirect para solucion de rutas sin HashRouter
    Agregar el fichero en la carpeta browser antes de desplegar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,10 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-
-    //HashStrategy
-    {
-      provide: LocationStrategy,
-      useClass: HashLocationStrategy
-    }
+    provideRouter(routes)
   ]
 };
