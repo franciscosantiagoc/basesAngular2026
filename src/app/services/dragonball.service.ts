fe706fb36569b658b59892053489d9cdb5c27dcf
Francisco Santiago <fcosantiagoc@gmail.com>
2026-08-28 03:17:29 -0600

    Feat (dragonball): Uso de effects para agregar persistencia con localstorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,21 +1,21 @@
-import { Service, signal } from '@angular/core';
+import { effect, Service, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem("characters");
+
+  return characters ?  JSON.parse(characters) : [];
+}
 
 //@Injectable(providedIn: 'root') Utilizar en caso de injeccion a traves de constructor
 @Service() //usar cuando usamos inyeccion de servicio con inject
 export class DragonballService {
-  characters = signal<Character[]>([
-    {
-      id: 1,
-      name: 'Goku',
-      power: 9001,
-    },
-    {
-      id: 2,
-      name: 'Vegeta',
-      power: 8600,
-    },
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(()=>{
+    console.log(`Character count list ${this.characters().length}`);
+    localStorage.setItem("characters", JSON.stringify(this.characters()));
+  })
 
   addCharacter(newCharacter: Character) {
     this.characters.update((characterArray) => [...characterArray, newCharacter]);
